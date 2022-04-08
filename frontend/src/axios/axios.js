2221538bad97e3000cf69078efa59c186fff17ab
YorgosKs <gkoutsikos1@gmail.com>
2022-04-08 14:58:39 +0300

    axios fix

--- frontend/src/axios/axios.js
+++ frontend/src/axios/axios.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
   // baseURL: 'http://localhost:5500/api/users',
-  baseURL: 'https://backend2-kgr8s.ondigitalocean.app',
+  baseURL: 'https://backend2-kgr8s.ondigitalocean.app/api/users',
 });
