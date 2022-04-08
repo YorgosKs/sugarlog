375e461e76566a75f7fcbe8bb8caa57eaa463d06
YorgosKs <gkoutsikos1@gmail.com>
2022-04-08 14:50:28 +0300

    axios fix #5

--- backend/index.js
+++ backend/index.js
@@ -45,7 +45,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: 'https://my-app-uy3g5.ondigitalocean.app/',
+    origin: 'https://my-app-uy3g5.ondigitalocean.app',
     methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
     credentials: true,
   })
