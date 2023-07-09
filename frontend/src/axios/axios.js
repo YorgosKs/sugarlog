5784933a713211e3b13dd3dfa99656b7061e7eaf
YorgosKs <gkoutsikos1@gmail.com>
2023-07-09 15:51:36 +0300

    axios url

--- frontend/src/axios/axios.js
+++ frontend/src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://backend2-kgr8s.ondigitalocean.app/api/users',
+  baseURL: 'https://backend-b94ud.ondigitalocean.app/api/users',
   // baseURL: 'http://127.0.0.1:5500/api/users',
   // baseURL: 'http://localhost:5500/api/users',
 });
